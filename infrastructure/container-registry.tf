5dab0ef6059d9d4796424cd6125d8fd319cfa4fe
Chris M <821688+tebriel@users.noreply.github.com>
2021-12-02 19:03:04 +0000

    Premium registry + build

--- infrastructure/container-registry.tf
+++ infrastructure/container-registry.tf
@@ -6,5 +6,5 @@ resource "azurerm_container_registry" "acr" {
     name = "FroduxContainerRegistry"
     resource_group_name = data.azurerm_resource_group.froudx-in.name
     location = data.azurerm_resource_group.froudx-in.location
-    sku = "Basic"
+    sku = "Premium"
 }
