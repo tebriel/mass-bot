cc1711cd5d9f4c985fbcdb732e909b0974d84917
Chris M <821688+tebriel@users.noreply.github.com>
2021-12-02 18:42:43 +0000

    Build a container registry

---
+++ infrastructure/container-registry.tf
@@ -0,0 +1,10 @@
+data "azurerm_resource_group" "froudx-in" {
+    name = "frodux.in"
+}
+
+resource "azurerm_container_registry" "acr" {
+    name = "FroduxContainerRegistry"
+    resource_group_name = data.azurerm_resource_group.froudx-in.name
+    location = data.azurerm_resource_group.froudx-in.location
+    sku = "Basic"
+}
