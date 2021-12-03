f514513ab88ca3cc8f5eb03ccb207ffd52fab334
Chris M <821688+tebriel@users.noreply.github.com>
2021-12-03 18:26:12 +0000

    Use service principal to deploy

--- infrastructure/mass-bot.tf
+++ infrastructure/mass-bot.tf
@@ -59,16 +59,27 @@ resource "azurerm_app_service" "mass-bot" {
   resource_group_name = azurerm_resource_group.mass-bot.name
   app_service_plan_id = azurerm_app_service_plan.mass-bot.id
 
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://${azurerm_container_registry.acr.login_server}"
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "BOT_GATEWAY_TOKEN"                   = ""
+  }
+
   site_config {
     acr_use_managed_identity_credentials = true
 
     always_on        = false
-    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/mass-bot/mass-bot:13781da32380adbadf0f1723b4b38bd05895c95f"
+    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/mass-bot/mass-bot:26e8134ff013d46b65bb825d42235dafacfefa10"
   }
 
   identity {
     type = "SystemAssigned"
   }
+
+  lifecycle {
+    ignore_changes = [site_config[0].linux_fx_version, app_settings["BOT_GATEWAY_TOKEN"]]
+  }
+
 }
 
 output "tfoutput" {
---
+++ script/gen_service_principal.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SUBSCRIPTION_ID=$(az account show --query id --output tsv)
+
+az ad sp create-for-rbac --name "mass-bot-sp" --role contributor \
+    --scopes /subscriptions/${SUBSCRIPTION_ID}/resourceGroups/mass-bot/providers/Microsoft.Web/sites/mass-bot \
+    --sdk-auth
