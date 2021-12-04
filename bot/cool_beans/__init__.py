7d39871c1fcd6ec505d941bd9a778efba018406a
Chris M <821688+tebriel@users.noreply.github.com>
2021-12-04 02:20:48 +0000

    Cool beans emoji

--- bot/cool_beans/__init__.py
+++ bot/cool_beans/__init__.py
@@ -6,7 +6,6 @@ DEFAULT_BEANS = 2
 
 async def handle(event: GuildMessageCreateEvent) -> None:
     mentions = event.message.mentions.users
-    print(event.author, mentions)
     await event.message.add_reaction("\N{burrito}")
     for mention in mentions.items():
         user = mention[1]
@@ -15,4 +14,4 @@ async def handle(event: GuildMessageCreateEvent) -> None:
             await event.message.respond("You can't beans yourself, you're not Buddy Capps!")
         else:
             beans = increment_beans(user.id, DEFAULT_BEANS)
-            await event.message.respond(f"{user.mention} was given {DEFAULT_BEANS} cool beans and now has {beans} :coolbeans:")
+            await event.message.respond(f"{user.mention} was given {DEFAULT_BEANS} cool beans and now has {beans} <:coolbeans:879772252609069117>")
